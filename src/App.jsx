7b54d59be59b09cb791774117097cab27471a814
Maricnka_Lozynska <masyakuk@gmail.com>
2024-11-07 07:57:13 +0200

    updating localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,16 @@ import { useState, useEffect} from 'react';
 import { nanoid } from 'nanoid'; 
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem('contacts');
+    return savedContacts ? JSON.parse(savedContacts) : [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  });
+
 
   const [filter, setFilter] = useState('');
 
@@ -33,11 +37,10 @@ function App() {
     setContacts((prevContacts) => [...prevContacts, { ...newContact, id: nanoid() }]);
   };
 
-
-  const deleteContact = (contactId) => {
-    setContacts((prevContact) => {
-      return prevContact.filter((contact) => contact.id !== contactId);
-    });
+const deleteContact = (contactId) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== contactId)
+    );
   };
 
     useEffect(() => {
